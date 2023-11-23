bf536470833f5ff0bd1342e075c1f4bf0d3b6f6e
daniinigogarcia98 <149794707+daniinigogarcia98@users.noreply.github.com>
2023-11-23 14:13:13 +0100

    ModificandoHolaMundo

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -2,12 +2,12 @@
 public class HolaMundo {
 
 	public static void main(String[] args) {
-		// TODO Esbozo de método generado automáticamente
+		
 		
 		System.out.println("Hola Mundo");
 		System.out.println("Bienvenidos...");
-		System.out.println("La suma de 7 más 2 es igual a: " + (7+2));
-
+		System.out.println("La suma de 7 m�s 2 es igual a: " + (7+2));
+		System.out.println("Hello i'm Daniel");
 	}
 
 }
